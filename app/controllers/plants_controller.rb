607905a795990db0e23a66e09d3c730dab00d0dc
carreuky <carreuky@gmail.com>
2022-09-26 15:04:18 +0300

    Done.

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,32 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  #DELETE /plant
+  def destroy 
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
+  end
+
+  #PATCH /plant
+  
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(is_in_stock: false)
+    render json: plant, status: :accepted
+  end
+
+  # def toggle
+  #   plant = plant.find_by(id: params[:id])
+  # if plant
+  #   plant.update(is_in_stock: false)
+  #   render json: plant
+  # else
+  #   render json: { error: "Blant not found" }, status: :not_found
+  # end
+  
+  # end
+
   private
 
   def plant_params
